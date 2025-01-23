cb3b81d874f33625845930da85cfae6f699bc9d6
a21ns1g4ts <atila.danvi@outlook.com>
2025-01-23 00:20:36 -0300

    Fix user class model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,76 @@
 
 namespace App\Models;
 
-use A2Insights\FilamentSaas\User\User as UserUser;
+use A2Insights\FilamentSaas\User\Settings;
+use Cog\Contracts\Ban\Bannable as BannableContract;
+use Cog\Laravel\Ban\Traits\Bannable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Jeffgreco13\FilamentBreezy\Traits\TwoFactorAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use TaylorNetwork\UsernameGenerator\FindSimilarUsernames;
 
-class User extends UserUser {}
+class User extends Authenticatable implements BannableContract, FilamentUser, MustVerifyEmail
+{
+    use Bannable,
+        FindSimilarUsernames,
+        HasApiTokens,
+        HasFactory,
+        HasRoles,
+        Notifiable,
+        SoftDeletes,
+        TwoFactorAuthenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'username',
+        'avatar_url',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'settings' => Settings::class,
+        ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'sysadmin') {
+            return $this->hasRole('super_admin');
+        }
+
+        return true;
+    }
+}
