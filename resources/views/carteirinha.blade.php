a284a3ca00f7ef983e2b89da29e44defcac6b0ba
a21ns1g4ts <atila.danvi@outlook.com>
2025-01-23 12:37:49 -0300

    Improve

--- resources/views/carteirinha.blade.php
+++ resources/views/carteirinha.blade.php
@@ -13,10 +13,10 @@
 
 <body style="width: 388px;height: 244px; position: relative; background-size: cover; background-repeat: no-repeat;">
     <div
-        style="position: absolute; bottom: 32px; left: -35px; font-size: 14px; font-family: sans-serif; line-height: 1.5;max-width:250px">
-        <h4 style="margin: 0;">{{ $nome }}</h4>
+        style="position: absolute; bottom: 32px; left: -35px; font-size: 14px; font-family: sans-serif; line-height: 1.4;max-width:250px">
+        <h4 style="margin: 0; margin-bottom:4px">{{ $nome }}</h4>
         <h4 style="margin: 0;">{{ $documento }}</h4>
-        <h4 style="margin: 0;">Validade: {{ $carteirinha->data_vencimento->format('d/m/Y') }}</h4>
+        <h4 style="margin: 0;">VALIDADE: {{ $carteirinha->data_vencimento->format('d/m/Y') }}</h4>
     </div>
     @if (isset($carteirinha->shortUrl->default_short_url))
         <div style="position: absolute; right: 100px; top: 142px; width: 65px; height: 65px;">
