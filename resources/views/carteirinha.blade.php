6c613d33d7e5b1e3068dbbafe9f7070bf6793cc8
a21ns1g4ts <atila.danvi@outlook.com>
2025-01-23 11:43:35 -0300

    Ajustes

--- resources/views/carteirinha.blade.php
+++ resources/views/carteirinha.blade.php
@@ -13,17 +13,17 @@
 
 <body style="width: 388px;height: 244px; position: relative; background-size: cover; background-repeat: no-repeat;">
     <div
-        style="position: absolute; bottom: 44px; left: -30px; font-size: 14px; font-family: sans-serif; line-height: 1.7;">
+        style="position: absolute; bottom: 44px; left: -35px; font-size: 14px; font-family: sans-serif; line-height: 2;">
         <h4 style="margin: 0;">{{ $nome }}</h4>
         <h4 style="margin: 0;">{{ $documento }}</h4>
         <h4 style="margin: 0;">Validade: {{ $carteirinha->data_vencimento->format('d/m/Y') }}</h4>
     </div>
     @if (isset($carteirinha->shortUrl->default_short_url))
-        <div style="position: absolute; right: 88px; top: 152px; width: 75px; height: 75px;">
+        <div style="position: absolute; right: 100px; top: 142px; width: 70px; height: 70px;">
             {{-- <p style="margin: 0; text-align: center;font-size: 10px;">
                 <small><strong>QR Code</strong></small>
             </p> --}}
-            <img style="width: 75px; height: 75px;" src="{{ $carteirinha->getQrCodeSvgUrl() }}">
+            <img style="width: 70px; height: 70px;" src="{{ $carteirinha->getQrCodeSvgUrl() }}">
             {{-- <p
                 style="margin: 0; text-align: center; font-size: 8px; max-width: 100%; word-wrap: break-word; overflow-wrap: break-word;">
                 {{ $carteirinha->shortUrl->default_short_url }}
