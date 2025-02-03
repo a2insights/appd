480fa2732ae1093fd3f627fa9ff79533c372cb73
a21ns1g4ts <atila.danvi@outlook.com>
2025-02-03 12:54:31 -0300

    Atendimento model migration with position adjustment

--- database/migrations/2025_02_03_101053_add_declaracao_to_atendimentos_table.php
+++ database/migrations/2025_02_03_101053_add_declaracao_to_atendimentos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('atendimentos', function (Blueprint $table) {
-            $table->json('declaracao')->nullable();
+            $table->json('declaracao')->nullable()->after('associado_id');
         });
     }
 
