1451e5a88e6ca5642e0312c0443169402da7c478
a21ns1g4ts <atila.danvi@outlook.com>
2025-03-31 09:49:55 -0300

    Add export functionality for Encaminhamentos with EncaminhamentoExporter

--- app/Filament/Resources/EncaminhamentoResource/Pages/ListEncaminhamentos.php
+++ app/Filament/Resources/EncaminhamentoResource/Pages/ListEncaminhamentos.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\EncaminhamentoResource\Pages;
 
+use App\Filament\Exports\EncaminhamentoExporter;
 use App\Filament\Resources\EncaminhamentoResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
@@ -13,6 +14,10 @@ class ListEncaminhamentos extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\ExportAction::make()
+                ->label('Exportar')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->exporter(EncaminhamentoExporter::class),
             Actions\CreateAction::make(),
         ];
     }
